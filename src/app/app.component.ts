4dc54bf6f09f012fc72f00152f3bb36672d0779a
tikun04 <toshiyokun@gmail.com>
2025-02-28 10:49:09 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Lab4';
+  title = 'data-binding-app';
+  count: number = 0;
+  blShow: boolean = false;
+  onButtonClick(){
+    this.count++;
+  }
+
+  myMethod(){
+    if (this.blShow){
+      this.blShow =false;
+      
+    }
+    else{
+      this.blShow = true;
+    }
+  }
 }
